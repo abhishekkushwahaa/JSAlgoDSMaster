b2b0048fdc0f7794219e0b5832835b8114c84a8c
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-04-16 21:08:23 +0530

    binary Search tree

---
+++ Trees/binarySearchTree.js
@@ -0,0 +1,117 @@
+// Implentaion of Binary Search Tree (BST) in JavaScript
+
+// Node class
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Binary Search Tree class
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // Insert a node
+  insert(data) {
+    let newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // Insert a node helper function
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Remove a node
+  remove(data) {
+    this.root = this.removeNode(this.root, data);
+  }
+
+  // Remove a node helper function
+  removeNode(node, key) {
+    if (node === null) {
+      return null;
+    } else if (key < node.data) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.data) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        return null;
+      } else if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      } else {
+        let aux = this.findMinNode(node.right);
+        node.data = aux.data;
+        node.right = this.removeNode(node.right, aux.data);
+        return node;
+      }
+    }
+  }
+
+  // Find the minimum node
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  // Inorder traversal
+  inorder(node) {
+    if (node !== null) {
+      this.inorder(node.left);
+      console.log(node.data);
+      this.inorder(node.right);
+    }
+  }
+
+  // Preorder traversal
+  preorder(node) {
+    if (node !== null) {
+      console.log(node.data);
+      this.preorder(node.left);
+      this.preorder(node.right);
+    }
+  }
+
+  // Postorder traversal
+  postorder(node) {
+    if (node !== null) {
+      this.postorder(node.left);
+      this.postorder(node.right);
+      console.log(node.data);
+    }
+  }
+
+  // Get the root node
+  getRootNode() {
+    return this.root;
+  }
+}
